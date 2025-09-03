59e5ae7903782f2d3d7d6efd02154644b0afbdfd
Franek Borowiec <franek.borowiec@gmail.com>
2025-09-03 10:11:02 +0200

    updated templates

--- clang_toolchain/templates/clang_wrapper.sh
+++ clang_toolchain/templates/clang_wrapper.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+# Clang itself needs host libraries to run
 export LD_LIBRARY_PATH="$SCRIPT_DIR/lib:$LD_LIBRARY_PATH"
 
-# check if linking command
-if [[ "$*" == *"-o"* ]] && [[ "$*" == *".o"* ]]; then
-	# use clang for linking with lld
-	exec "$SCRIPT_DIR/bin/clang" -fuse-ld=lld "$@"
-else
-	# regular compilation
-	exec "$SCRIPT_DIR/bin/clang" "$@"
-fi
+exec "$SCRIPT_DIR/bin/clang" "$@"
